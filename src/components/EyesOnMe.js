c3416c2ed68906acebfcfa83c4a5142aaf7d405f
yurubahmed <yurubiftin2002@gmail.com>
2023-01-14 15:31:45 -0500

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe (){
+    function handleFocus(){
+        console.log("Good!");
+    }
+    function handleBlur(){
+        console.log("Hey!Eyes On Me!")
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
+    );  
+        
+}
+ 
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...");
+        console.log(event.target.value);
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange}/>
+        </div>
     )
 }
 
